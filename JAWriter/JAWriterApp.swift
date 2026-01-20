bc452d6a7a6d0fe8b9e4c7dc3336746ce79f9458
Jake Bloom <gday@jakebloom.me>
2026-01-19 21:21:12 -0800

    Initial Commit

---
+++ JAWriter/JAWriterApp.swift
@@ -0,0 +1,32 @@
+//
+//  JAWriterApp.swift
+//  JAWriter
+//
+//  Created by Jake on 19/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct JAWriterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
